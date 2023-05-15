8ba2d4044358b3501c8cf90f2d7d99a152dff95f
VinCoD <5895.2020@students.ku.ac.ke>
2023-05-15 16:37:37 +0300

    Modified the admin page using ModelAdmin class and list_display tuple

--- books/admin.py
+++ books/admin.py
@@ -3,4 +3,16 @@ from .models import Book
 
 # Register your models here.
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  # This class inherits from the admin.ModelAdmin class.
+
+  list_display = (
+    "title",
+    "subtitle",
+    "author",
+    "isbn"
+  )
+  # This attribute specifies the fields that will be displayed in the list view for the Todo model.
+
+admin.site.register(Book, BookAdmin)
+
